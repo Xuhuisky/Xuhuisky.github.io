0c523ec828eec6cc754f878dc2a713d3e574bd0b
Xuhuisky <ganhanyu@yeah.net>
2024-10-07 15:55:55 -0400

    website initialization

---
+++ static/js/daysRemaining.js
@@ -0,0 +1,16 @@
+function calculateDaysRemaining() {
+            const today = new Date();
+
+            document.querySelectorAll('.due-date').forEach(function (element) {
+                const dueDateStr = element.dataset.dueDate;
+                const dueDate = new Date(dueDateStr);
+                const timeDiff = dueDate.getTime() - today.getTime();
+                const daysRemaining = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+                // Show the countdown result with application due date
+                element.innerHTML = dueDateStr + " (" + (daysRemaining >= 0 ? daysRemaining + " days remaining" : "Past Due") + ")";
+            });
+        }
+
+        // Run the function after the DOM content is loaded
+        document.addEventListener('DOMContentLoaded', calculateDaysRemaining);
