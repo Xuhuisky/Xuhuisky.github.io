0b1474574de7542f72dcded166768d4d4bd21037
Xuhuisky <ganhanyu@yeah.net>
2024-10-11 12:15:09 -0400

    try to make title of image centered 2nd time

--- static/G-GRE-Verbal js/random_image.js
+++ static/G-GRE-Verbal js/random_image.js
@@ -19,12 +19,14 @@ function displayRandomImage() {
             // Update all elements with the class '.random-image'
             document.querySelectorAll('.random-image').forEach(function(element) {
                 element.innerHTML = `
-                <li>
-                    <a href="/g-gre-verbal${linkUrl}.html">
-                        <img src="${imageUrl}" alt="Image for ${imageName}" style="width: 600px; height: auto;">
-                    </a>
-                    <p>${imageName}</p>
-                </li>
+                <ul>
+                    <li>
+                        <a href="/g-gre-verbal${linkUrl}.html">
+                            <img src="${imageUrl}" alt="Image for ${imageName}" style="width: 600px; height: auto;">
+                        </a>
+                        <p>${imageName}</p>
+                    </li>
+                </ul>
                 `;
             });
         })
